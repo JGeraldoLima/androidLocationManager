f9cd04db39cc04e564b344c94cabff6788047b11
jgeraldo <j.gbaixinho@gmail.com>
2016-07-03 22:48:20 -0300

    Improved UI

--- androidLocationManager/app/src/main/java/locationmanager/jgeraldo/com/androidlocationmanager/activities/MainActivity.java
+++ androidLocationManager/app/src/main/java/locationmanager/jgeraldo/com/androidlocationmanager/activities/MainActivity.java
@@ -4,13 +4,10 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -55,18 +52,9 @@ public class MainActivity extends AppCompatActivity
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+                this, drawer, mToolbar, 0, 0);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
@@ -131,12 +119,8 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
--- androidLocationManager/app/src/main/java/locationmanager/jgeraldo/com/androidlocationmanager/ui/fragments/MapsFragment.java
+++ androidLocationManager/app/src/main/java/locationmanager/jgeraldo/com/androidlocationmanager/ui/fragments/MapsFragment.java
@@ -2,12 +2,12 @@ package locationmanager.jgeraldo.com.androidlocationmanager.ui.fragments;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -15,9 +15,12 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import io.github.yavski.fabspeeddial.FabSpeedDial;
+import io.github.yavski.fabspeeddial.SimpleMenuListenerAdapter;
 import locationmanager.jgeraldo.com.androidlocationmanager.R;
 
 public class MapsFragment extends Fragment implements OnMapReadyCallback {
@@ -29,6 +32,8 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
     private static Toolbar mToolbar;
 
     private static FragmentManager mFragmentManager;
+
+    private static FabSpeedDial mFabAdd;
 
     private View view;
 
@@ -52,13 +57,13 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        mToolbar.setTitle("Map"/*R.string.menu_profile*/);
+        mToolbar.setTitle(R.string.nav_map);
         if (view == null) {
-            Toolbar toolbar = (Toolbar) mActivity.findViewById(R.id.toolbar);
             view = inflater.inflate(R.layout.fragment_map, container, false);
 
-            SupportMapFragment mapFragment = (SupportMapFragment) mFragmentManager.findFragmentById(R.id.map);
-            mapFragment.getMapAsync(this);
+            setFabActions();
+            setUpMap();
+
         } else {
             ViewGroup parent = (ViewGroup) view.getParent();
             if (parent != null) {
@@ -68,13 +73,34 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
         return view;
     }
 
+    private void setFabActions() {
+        mFabAdd = (FabSpeedDial) view.findViewById(R.id.fab_map);
+        mFabAdd.setMenuListener(new SimpleMenuListenerAdapter() {
+            @Override
+            public boolean onMenuItemSelected(MenuItem menuItem) {
+                int itemId = menuItem.getItemId();
+
+                if (itemId == R.id.ic_save_current_location) {
+                    return true;
+                } else if (itemId == R.id.ic_goto_my_position) {
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    private void setUpMap() {
+        SupportMapFragment mapFragment = (SupportMapFragment) mFragmentManager.findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         LatLng campina = new LatLng(-7.2190974, -35.903685);
-        mMap.addMarker(new MarkerOptions().position(campina).title("Marker in Campina Grande"));
+        mMap.addMarker(new MarkerOptions().position(campina).title("Marker in Campina Grande").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_my_location)));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(campina, 16.0f));
     }
-
 }
